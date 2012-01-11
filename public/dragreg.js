e43fee454196679a1eabd08f2420cbf1bd6d0fec
Paul van Tilburg <paul@luon.net>
2012-01-11 23:31:07 +0100

    Get the current draggables and save the positions from the client

--- public/dragreg.js
+++ public/dragreg.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $("#add").click(show_add_dialog);
   $("#add_form #cancel").click(hide_add_dialog);
+
+  $.get("current", function(data) {
+    $.each(data, function(key, val) {
+       $.get("draggable/" + key, function(data) {
+         $("#draggables").append(data);
+         $(".draggable").draggable({ stack: "#draggables img",
+                                     stop: update_drag_info });
+       })
+    })
+  }, "json");
 });
 
 function show_add_dialog() {
@@ -12,3 +22,7 @@ function hide_add_dialog() {
     $("#add_form")[0].reset();
   });
 };
+
+function update_drag_info(event, ui) {
+  $.post("savepos/" + event.srcElement.id, ui.position, "json");
+}
