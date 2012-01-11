3006aed3ef49757786856dcda2c4be808c95a5c3
Paul van Tilburg <paul@luon.net>
2012-01-11 23:26:58 +0100

    Remove trailing spaces

--- plemp.js
+++ plemp.js
@@ -1,10 +1,10 @@
-// plemp.rb - The Plemp! application, create your own on-line pile of junk! 
-//  
+// plemp.rb - The Plemp! application, create your own on-line pile of junk!
+//
 // Plemp! is Copyright © 2012 Paul van Tilburg <paul@mozcode.nl>
-//  
-// This program is free software; you can redistribute it and/or modify it under 
-// the terms of the GNU General Public License as published by the Free Software 
-// Foundation; either version 2 of the License, or (at your option) any later  
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
 // version.
 
 var express = require("express"),
